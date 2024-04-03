e34572c83e61076a2add5246d38e916e13e239b6
Ike Gabriel Yuson <iggyyuson09@gmail.com>
2024-04-04 01:53:49 +0800

    init

---
+++ BlogMaker/blog_backend/factories.py
@@ -0,0 +1,12 @@
+from factory import Faker
+from factory.django import DjangoModelFactory
+
+from .models import BlogPost
+
+
+class BlogPostFactory(DjangoModelFactory):
+    class Meta:
+        model = BlogPost
+
+    title = Faker("sentence")
+    content = Faker("text")
